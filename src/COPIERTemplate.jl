1d1dbc2f34e8c850145ab877724474c37d194520
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-05-24 11:07:27 +0200

    Add copy, recopy and update from copier and change the API
    Adds copy, recopy, and update, following the copier API.
    Change generate to accept a positional argument `data`.
    
    BREAKING CHANGES: generate has a different API now
    
    Closes #136

--- src/COPIERTemplate.jl
+++ src/COPIERTemplate.jl
@@ -1,3 +1,11 @@
+"""
+# COPIERTemplate.jl
+
+This package defines a copier template for Julia packages and a basic user interface aroud copier
+to use it from Julia.
+
+The main functions are: [`generate`](@ref)
+"""
 module COPIERTemplate
 
 using PythonCall, CondaPkg, UUIDs
@@ -7,24 +15,60 @@ function __init__()
 end
 
 """
-    generate(dst_path; kwargs...)
-    generate(src_path, dst_path; kwargs...)
+    copy(dst_path[, data]; kwargs...)
+    copy(src_path, dst_path[, data]; kwargs...)
 
-Runs the `copy` command of [copier](https://github.com/copier-org/copier) with the COPIERTemplate template.
-
-If `src_path` is not informed, the GitHub URL of COPIERTemplate.jl is used.
-
-Even though the template is available offline through this template, this uses the github URL to allow updating.
-
-The keyword arguments are passed directly to the `run_copy` function of `copier`.
+Wrapper around Python's [copier.run_copy](https://copier.readthedocs.io/en/stable/reference/main/#copier.main.run_copy).
+Use [`generate`](@ref) for a more user-friendly function.
 """
-function generate(src_path, dst_path; kwargs...)
+function Base.copy(src_path, dst_path, data::Dict = Dict(); kwargs...)
   copier = PythonCall.pyimport("copier")
-  copier.run_copy(src_path, dst_path; kwargs..., vcs_ref = "HEAD")
+  copier.run_copy(src_path, dst_path, data; kwargs...)
 end
 
-function generate(dst_path, args...; kwargs...)
-  generate("https://github.com/abelsiqueira/COPIERTemplate.jl", dst_path, args...; kwargs...)
+function Base.copy(dst_path, data::Dict = Dict(); kwargs...)
+  copy(joinpath(@__DIR__, ".."), dst_path, data; kwargs...)
+end
+
+"""
+    recopy(dst_path[, data]; kwargs...)
+
+Wrapper around Python's [copier.run_recopy](https://copier.readthedocs.io/en/stable/reference/main/#copier.main.run_recopy).
+"""
+function recopy(dst_path, data::Dict = Dict(); kwargs...)
+  copier = PythonCall.pyimport("copier")
+  copier.run_recopy(dst_path, data; kwargs...)
+end
+
+"""
+    update(dst_path[, data]; kwargs...)
+
+Wrapper around Python's [copier.run_update](https://copier.readthedocs.io/en/stable/reference/main/#copier.main.run_update).
+"""
+function update(dst_path, data::Dict = Dict(); kwargs...)
+  copier = PythonCall.pyimport("copier")
+  copier.run_update(dst_path, data; kwargs...)
+end
+
+"""
+    generate(dst_path[, data]; kwargs...)
+    generate(src_path, dst_path[, data]; true, kwargs...)
+
+Runs the `copy` command of [copier](https://github.com/copier-org/copier) with the COPIERTemplate template.
+If `src_path` is not informed, the GitHub URL of COPIERTemplate.jl is used.
+
+The `data` argument is a dictionary of answers (values) to questions (keys) that can be used to bypass some of the interactive questions.
+
+## Keyword arguments
+
+The keyword arguments are passed directly to [`copy`](@ref).
+"""
+function generate(src_path, dst_path, data::Dict = Dict(); kwargs...)
+  copy(src_path, dst_path, data; kwargs...)
+end
+
+function generate(dst_path, data::Dict = Dict(); kwargs...)
+  generate("https://github.com/abelsiqueira/COPIERTemplate.jl", dst_path, data; kwargs...)
 end
 
 end
