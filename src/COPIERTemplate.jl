f207dbd75d3ea937cb3acb4d1724a398b5a47df4
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-05-30 12:49:08 +0200

    Allow optional src_path argument to generate
    If the argument is passed, copy from that location, otherwise
    url the URL.
    
    BREAKING CHANGE: The default API now clones from the URL, which means
    that even if you clone the repo and run the generate command, it will
    use the latest tagged version.

--- src/COPIERTemplate.jl
+++ src/COPIERTemplate.jl
@@ -7,21 +7,29 @@ function __init__()
 end
 
 """
-    generate(path, generate_missing_uuid = true; kwargs...)
+    generate(dst_path, generate_missing_uuid = true; kwargs...)
+    generate(src_path, dst_path, generate_missing_uuid = true; kwargs...)
 
 Runs the `copy` command of [copier](https://github.com/copier-org/copier) with the COPIERTemplate template.
+
+If `src_path` is not informed, the GitHub URL of COPIERTemplate.jl is used.
+
 Even though the template is available offline through this template, this uses the github URL to allow updating.
 
 The keyword arguments are passed directly to the `run_copy` function of `copier`.
 If `generate_missing_uuid` is `true` and there is no `kwargs[:data]["PackageUUID"]`, then a UUID is generated for the package.
 """
-function generate(path, generate_missing_uuid = true; kwargs...)
+function generate(src_path, dst_path, generate_missing_uuid = true; kwargs...)
   copier = PythonCall.pyimport("copier")
   data = copy(get(kwargs, :data, Dict()))
   if generate_missing_uuid && !("PackageUUID" in keys(data))
     data["PackageUUID"] = string(uuid4())
   end
-  copier.run_copy("https://github.com/abelsiqueira/COPIERTemplate.jl", path; kwargs..., data = data, vcs_ref = "HEAD")
+  copier.run_copy(src_path, dst_path; kwargs..., data = data, vcs_ref = "HEAD")
+end
+
+function generate(dst_path, args...; kwargs...)
+  generate("https://github.com/abelsiqueira/COPIERTemplate.jl", dst_path, args...; kwargs...)
 end
 
 end
