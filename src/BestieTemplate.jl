131d890174852f197adc4538c63ebce5af2e7c75
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-07-02 15:01:33 +0200

    Update module docs

--- src/BestieTemplate.jl
+++ src/BestieTemplate.jl
@@ -4,7 +4,7 @@
 This package defines a copier template for Julia packages and a basic user interface aroud copier
 to use it from Julia.
 
-The main functions are: [`generate`](@ref)
+The main functions are: [`generate`](@ref) and [`update`](@ref).
 """
 module BestieTemplate
 
