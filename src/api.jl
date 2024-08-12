b29b69b42c1c64d80837c34d2da88ce7ca86bd22
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-08-12 15:30:26 +0200

    Apply BestieTemplate 0.9.1

--- src/api.jl
+++ src/api.jl
@@ -15,26 +15,28 @@ function _copy(src_path, dst_path, data; kwargs...)
     end
   end
 
-  quiet || display(md"""Hi, **❤ Bestie ❤** here.
+  quiet || display(
+    md"""Hi, **❤ Bestie ❤** here.
 
-        Below you will find a few questions to configure your template.
-        First, some **required** questions will need to be filled.
-        Then, you will have the option of selecting
+  Below you will find a few questions to configure your template.
+  First, some **required** questions will need to be filled.
+  Then, you will have the option of selecting
 
-        - The _recommended_ options, which includes our current _best practices recommendations_;
-        - The _minimum_ options, which will answer _no_ to everything, but still give you what
-          we consider the minimum best practices you need to get started; or
-        - Answer every optional question.
+  - The _recommended_ options, which includes our current _best practices recommendations_;
+  - The _minimum_ options, which will answer _no_ to everything, but still give you what
+    we consider the minimum best practices you need to get started; or
+  - Answer every optional question.
 
-        On any case, we suggest reading the **full guide**, and possibly other documentation pages:
+  On any case, we suggest reading the **full guide**, and possibly other documentation pages:
 
-        `https://abelsiqueira.com/BestieTemplate.jl/stable/10-full-guide`
+  `https://abelsiqueira.com/BestieTemplate.jl/stable/10-full-guide`
 
-        If something does not work as you would expect or you need clarifications,
-        please open an issue or discussion.
+  If something does not work as you would expect or you need clarifications,
+  please open an issue or discussion.
 
-        **❤ Good luck filling the questions, and thanks for choosing BestieTemplate ❤**
-      """)
+  **❤ Good luck filling the questions, and thanks for choosing BestieTemplate ❤**
+""",
+  )
   Copier.copy(src_path, dst_path, data; kwargs...)
 end
 
