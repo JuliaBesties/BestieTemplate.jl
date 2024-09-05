f7e5b438970362fc8c2d077ac06060d76a5e2539
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-09-05 15:14:31 +0200

    Use a variable to improve readibility of docs/make.jl
    Closes #351

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,10 @@ using Documenter
 DocMeta.setdocmeta!(BestieTemplate, :DocTestSetup, :(using BestieTemplate); recursive = true)
 
 const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
+const numbered_pages = [
+  file for
+  file in readdir(joinpath(@__DIR__, "src")) if file != "index.md" && splitext(file)[2] == ".md"
+]
 
 makedocs(;
   modules = [BestieTemplate],
@@ -11,13 +15,7 @@ makedocs(;
   repo = "https://github.com/abelsiqueira/BestieTemplate.jl/blob/{commit}{path}#{line}",
   sitename = "BestieTemplate.jl",
   format = Documenter.HTML(; canonical = "https://abelsiqueira.github.io/BestieTemplate.jl"),
-  pages = [
-    "index.md"
-    [
-      file for
-      file in readdir(joinpath(@__DIR__, "src")) if file != "index.md" && splitext(file)[2] == ".md"
-    ]
-  ],
+  pages = ["index.md"; numbered_pages],
 )
 
 deploydocs(; repo = "github.com/abelsiqueira/BestieTemplate.jl")
